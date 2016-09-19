c71e62f4d88cd5a18791fc61ed34eeb1a29a82d9
canlasm <michael.canlas@foxnews.com>
2016-09-19 17:02:59 -0400

    move original test to __tests__ folder

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,9 @@
 // USEFUL LINKS
 // https://facebook.github.io/jest/
 // https://github.com/ironprice91/countdown-timer#running-tests
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
 
 // example test
 test("adds 1 + 2 to equal 3", () => {
@@ -8,4 +11,9 @@ test("adds 1 + 2 to equal 3", () => {
 		return a + b;
 	}
 	expect(sum(1,2)).toBe(3);
+});
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
